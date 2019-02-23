b88eb63593b4a4cca8fa16c6470502853a0cd9c1
neyser zana <neyserj.zana@gmail.com>
2019-02-23 01:26:11 -0500

    preparing routes to serve static pages

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const router = express.Router();
+
+const path = __dirname + '/views/';
+const port = 8080;
+
+router.use(function(req, res, next){
+  console.log('/' + req.method);
+  next();
+});
+
+router.get('/', function(req, res){
+  res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks', function(req, res){
+  res.sendFile(path + 'sharks.html');
+});
 
 
 app.listen(port, () => {
